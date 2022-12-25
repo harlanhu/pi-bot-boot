18168073aa807326d97ca3cd37c6b07fc70cb877
harlan <isharlan.hu@gmail.com>
2022-12-25 18:33:51 +0800

    update

--- src/main/java/cn/tpkf/bot/PiBotApplication.java
+++ src/main/java/cn/tpkf/bot/PiBotApplication.java
@@ -1,9 +1,7 @@
 package cn.tpkf.bot;
 
-import cn.tpkf.bot.utils.SpringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author Harlan
@@ -12,8 +10,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class PiBotApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(PiBotApplication.class, args);
-        SpringUtils.setAppContext(context);
+        SpringApplication.run(PiBotApplication.class, args);
     }
 
 }
